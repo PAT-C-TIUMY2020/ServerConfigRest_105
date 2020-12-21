00bef496368c3c876bcee62f2aa10b38b3248902
Riefkha Ananda Putri <riefkhaanandaputri354@gmail.com>
2020-12-21 14:29:27 +0700

    Add project files.

---
+++ ServerConfigRest_20180140105_RiefkhaAnandaPutri/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_20180140105_RiefkhaAnandaPutri;
+
+namespace ServerConfigRest_20180140105_RiefkhaAnandaPutri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
